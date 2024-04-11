12805b084d02d80bcde7c0949ea9071ba5b7214a
mrfabroa <eric.fabroa@ycdsbk12.ca>
2024-04-11 11:44:32 -0400

    update

--- Program1.java
+++ Program1.java
@@ -1,4 +1,4 @@
-class Main extends ConsoleProgram {
+class Program1 extends ConsoleProgram {
 
   /**
   * Description
@@ -7,15 +7,20 @@ class Main extends ConsoleProgram {
   
   public void run() {
     
-    // start coding here
-    Double dblTest = readDouble("Enter in a double (but don't):");
-    System.out.println(dblTest);
-    
+    // start coding here    
     String combined = combineStr("Hello", "World");
     System.out.println(combined);
+
+    System.out.println("The square root of 4 is " + mySqrt(4) + "." );
   }
 
-  public String combineStr(String str1, String str2){
+  private String combineStr(String str1, String str2){
     return str1+str2;
   }
+
+  private double mySqrt(double dblSquare){
+
+    return Math.pow(dblSquare,0.5);
+  }
+
 }
